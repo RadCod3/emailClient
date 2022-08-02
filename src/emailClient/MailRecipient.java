cf32879e9a8c57942bc55fba81cf4138e38c53e7
RadCod3 <jjeli1234@gmail.com>
2022-08-03 01:10:09 +0530

    Removed commented unused field

--- src/emailClient/MailRecipient.java
+++ src/emailClient/MailRecipient.java
@@ -10,7 +10,6 @@ public abstract class MailRecipient {
     private static int count = 0;
     private String name;
     private InternetAddress emailAddress;
-    // private Date birthday;
 
     public MailRecipient(String name, InternetAddress emailAddress) {
         this.name = name;
